7f14a2b829e2f0c0dc6858cbf341ba7034b91617
George Lykuanov <dissfall@fastmail.com>
2021-04-26 22:48:25 +0300

    build: configure bundling

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,6 +2,6 @@ module.exports = {
   "*.md,!test/**/*.md": [
     filenames => filenames.map(filename => `remark ${filename} -fo`)
   ],
-  'package.json': 'fixpack',
-  '*.ts': 'xo --fix'
+  '*.ts': 'xo --fix',
+  'package.json': 'fixpack'
 };
